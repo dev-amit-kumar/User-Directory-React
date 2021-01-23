df38952ed814663bad96213495533b1d6fcfc27e
Amit Kumar <amitkumar.developer1@gmail.com>
2021-01-23 05:41:24 +0530

    Updated loader

--- attainu-assignment/src/Components/User/UserSection.jsx
+++ attainu-assignment/src/Components/User/UserSection.jsx
@@ -1,16 +1,20 @@
 import UserDetail from './UserDetail';
 
 const UserSection = (props) => {
-	if (props.data) {
+	if (props.data && props.data.length > 0) {
 		return props.data.map((user, idx) => {
 			return <UserDetail userData={user} key={idx} />;
 		});
-	} else {
+	} else if (!props.data) {
 		return (
-			<div>
-				<h3>No user data found</h3>
+			<div class="d-flex justify-content-center">
+				<div class="spinner-border" role="status">
+					<span class="sr-only">Loading...</span>
+				</div>
 			</div>
 		);
+	} else {
+		return <h1>No user data found</h1>;
 	}
 };
 
